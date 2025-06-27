8846f437ff008e136c074823232430f1716bd72f
Eduard Ursu <eduardursu78@gmail.com>
2025-06-27 14:21:44 +0200

    overall fixes

--- config/config.js
+++ config/config.js
@@ -1,19 +1,12 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-    src: path.resolve(__dirname, '../src'),
-    build: path.resolve(__dirname, '../dist'),
-    extBuild: path.resolve(__dirname, '../dist-ext'),
-    extJS: ['blank'],
-    extPages: ['blank'],
-    extV3JS: ['blank'],
-    ext: path.resolve(__dirname, '../dist-ext'),
-    public: path.resolve(__dirname, '../src'),
-    type: process.env.TYPE,
-	mv: process.env.MV,
-    pages: ['index', 'themes']
-}
-
-	
-	
-	
-	
+  src: path.resolve(__dirname, "../src"),
+  build: path.resolve(__dirname, "../dist"),
+  extBuild: path.resolve(__dirname, "../dist-ext"),
+  extJS: ["blank"],
+  extPages: ["blank"],
+  ext: path.resolve(__dirname, "../dist-ext"),
+  public: path.resolve(__dirname, "../src"),
+  type: process.env.TYPE,
+  pages: ["index", "themes"],
+};
